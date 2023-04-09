8cdd1ac181cafa75efd55a800fd15650013d8b7f
Дмитрий Бондаренко <wiggleeeeee@mail.ru>
2023-04-09 16:47:42 +0300

    fixed

--- app/js/main.js
+++ app/js/main.js
@@ -5,14 +5,25 @@ $(document).ready(function () {
 
 		if (value.val().length > 0) {
 			close.addClass('length')
+			$('.header__search').addClass('active')
 		} else {
 			close.removeClass('length')
+			$('.header__search').removeClass('active')
 		}
+	})
+
+	$('.header__mobile--search').on('click', function(){
+		$('.modal-search').toggleClass('active')
+	})
+
+	$('.modal-search__close').on('click', function(){
+		$('.modal-search').removeClass('active')
 	})
 
 	$('.field__close').on('click', function () {
 		$('.header__search .field input').val('')
 		$(this).removeClass('length')
+		$('.header__search').removeClass('active')
 	})
 
 	$('.nav__burger').on('click', function () {
@@ -121,7 +132,10 @@ $(document).ready(function () {
 	})
 
 	$(document).on('click', function (e) {
-		if (!$(e.target).closest('.header__mobile--burger, .header-video__burger').length) {
+		if (
+			!$(e.target).closest('.header__mobile--burger, .header-video__burger')
+				.length
+		) {
 			$('.nav').removeClass('active')
 			$('.header-video__burger').removeClass('active')
 		}
@@ -130,5 +144,97 @@ $(document).ready(function () {
 
 	$('.card__dots').on('click', function () {
 		$(this).toggleClass('active')
+	})
+
+	$('.video__later').on('click', function () {
+		$(this).toggleClass('active')
+	})
+
+	$('.video__like').on('click', function () {
+		$(this).toggleClass('active')
+		$(this).next().removeClass('active')
+	})
+
+	$('.video__dislike').on('click', function () {
+		$(this).toggleClass('active')
+		$(this).prev().removeClass('active')
+	})
+
+	$('.video__repost').on('click', function () {
+		$(this).toggleClass('active')
+		$(this).next().toggleClass('active')
+		if ($('.video__repost-dropdown').hasClass('active')) {
+			$(this).parent().toggleClass('active')
+		} else {
+			$('.video__repost-wrapper').removeClass('active')
+		}
+	})
+
+	$(document).on('click', function (e) {
+		if (
+			!$(e.target).closest('.video__repost, .video__repost-dropdown').length
+		) {
+			$('.video__repost-dropdown').removeClass('active')
+			$('.video__repost').removeClass('active')
+			$('.video__repost-wrapper').removeClass('active')
+		}
+		e.stopPropagation()
+	})
+
+	$('.video__dots').on('click', function () {
+		$(this).toggleClass('active')
+		$(this).next().toggleClass('active')
+		if ($('.video__dots-dropdown').hasClass('active')) {
+			$(this).parent().toggleClass('active')
+		} else {
+			$('.video__dots-wrapper').removeClass('active')
+		}
+	})
+
+	$(document).on('click', function (e) {
+		if (!$(e.target).closest('.video__dots, .video__dots-dropdown').length) {
+			$('.video__dots-dropdown').removeClass('active')
+			$('.video__dots').removeClass('active')
+			$('.video__dots-wrapper').removeClass('active')
+		}
+		e.stopPropagation()
+	})
+
+	if ($(window).width() < 992) {
+		$(window).scroll(function () {
+			var aTop = $('.header-video').height()
+			if ($(this).scrollTop() >= aTop) {
+				$('.video').addClass('sticky')
+			} else {
+				$('.video').removeClass('sticky')
+			}
+		})
+	}
+
+	$('.comments__icon').on('click', function () {
+		var bodyHeight = $(window).height()
+		var videoHeight = $('.video__img').height()
+		var headerHeight = $('.header-video').height()
+
+		$('.comments__mobile').toggleClass('active')
+
+		if ($('.comments__mobile').hasClass('active')) {
+			if ($('.video').hasClass('sticky')) {
+				$('.comments__mobile').css(
+					'height',
+					`${bodyHeight - videoHeight - headerHeight}`
+				)
+			} else {
+				$('.comments__mobile').css(
+					'height',
+					`${bodyHeight - videoHeight - headerHeight - '22'}`
+				)
+			}
+
+			$('body').addClass('scroll')
+		} else {
+			$('.comments__mobile').css('height', 'auto')
+			$('body').removeClass('scroll')
+		}
 	})
 })
